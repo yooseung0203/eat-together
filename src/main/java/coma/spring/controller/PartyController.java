51bf7893858ff240caf10d1f1f62bf480d7d2f70
KimTaeHoon43 <60641563+KimTaeHoon43@users.noreply.github.com>
2020-07-06 09:55:13 +0900

    Update PartyController.java
    delete modal

--- src/main/java/coma/spring/controller/PartyController.java
+++ src/main/java/coma/spring/controller/PartyController.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
